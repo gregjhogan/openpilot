154233a16121c2b694a2505a6de3dd26e3264e5d
Greg Hogan <gregjhogan@gmail.com>
2018-03-18 20:16:54 -0500

    disable annoying logging

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -427,7 +427,7 @@ def manager_thread():
 
     location = location.gpsLocation if location else None
 
-    print td
+    #print td
 
     # replace thermald
     msg = read_thermal()
@@ -456,7 +456,7 @@ def manager_thread():
     msg.thermal.startedTs = int(1e9*(started_ts or 0))
 
     thermal_sock.send(msg.to_bytes())
-    print msg
+    #print msg
 
     # uploader is gated based on the phone temperature
     if max_temp > 85.0:
@@ -530,8 +530,8 @@ def manager_thread():
         os.system('LD_LIBRARY_PATH="" svc power shutdown')
 
     # check the status of all processes, did any of them die?
-    for p in running:
-      cloudlog.debug("   running %s %s" % (p, running[p]))
+    #for p in running:
+    #  cloudlog.debug("   running %s %s" % (p, running[p]))
 
     # report to server once per minute
     if (count%60) == 0:
